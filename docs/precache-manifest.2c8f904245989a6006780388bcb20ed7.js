e30453ebeeb9bf15919585527f4058a202c3b295
Clara Brígido <clarabrigido@gmail.com>
2019-09-24 14:11:55 +0200

    Add docs

---
+++ docs/precache-manifest.2c8f904245989a6006780388bcb20ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0d12cbbbfc0a315949f14124f74148",
+    "url": "./index.html"
+  },
+  {
+    "revision": "289ccac581946ee11316",
+    "url": "./static/css/main.beb46b89.chunk.css"
+  },
+  {
+    "revision": "0c6ad23915d445f5ea52",
+    "url": "./static/js/2.26facb0c.chunk.js"
+  },
+  {
+    "revision": "289ccac581946ee11316",
+    "url": "./static/js/main.eb904dba.chunk.js"
+  },
+  {
+    "revision": "e2750da514b50e7de160",
+    "url": "./static/js/runtime-main.26da6e4f.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
